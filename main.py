66117477d1b506f51044b8213df7719a7505faad
KowalczukKow <jakub.kowalczukp@gmail.com>
2026-02-28 14:43:24 +0100

    ajout de main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import re
+from reconaissance import expr
+
+def main():
+    print("hello")
+
+    automate = re.compile(expr)
+
+    # verifie si les mots étaient reconnus
+    with open("corpus/small.brown", "r", encoding="utf-8") as f:
+        contenu = f.read()
+        valides = automate.findall(contenu)
+        motsTotal = contenu.split()
+        print("Mots reconnus: "+ str(len(valides)))
+        print("Mots en total: "+str(len(motsTotal)))
+        pourcentage = len(valides)/len(motsTotal) * 100
+        print("Pourcentage des mots bien annotés: " + str(pourcentage) + "%")
+
+
+if __name__ == "__main__":
+    main()
--- reconaissance.py
+++ reconaissance.py
@@ -37,26 +37,9 @@ et = "ET" #mot étranger
 ponct = "PONCT" 
 
 pd = "P\+D"
-#faut traiter P+D ex. au
 
 expr = (mot + "(?:"+  det + "|" + nc + "|" + npp + "|" 
     + vinf + "|" + vpr + "|" + vpp + "|" + v + "|"
     + advwh + "|"  + adj + "|" + adv + "|" + et + "|"
     + cc + "|" + cs + "|" + cls + "|" + clo + "|" + clr + "|"
     + ponct + "|" + prorel + "|" + pro + "|" + pref + "|" + pd + "|" + p +")" )
-
-automate = re.compile(expr)
-
-# verifie si les mots étaient reconnus
-with open("corpus/small.brown", "r", encoding="utf-8") as f:
-    contenu = f.read()
-    valides = automate.findall(contenu)
-    motsTotal = contenu.split()
-    print("Mots reconnus: "+ str(len(valides)))
-    print("Mots en total: "+str(len(motsTotal)))
-    pourcentage = len(valides)/len(motsTotal) * 100
-    print("Pourcentage des mots bien annotés: " + str(pourcentage) + "%")
-
-exclus = re.sub(expr, "", contenu) # ce qui nest pas inclu dans expr
-print(exclus)
-#print(valides[:100])
