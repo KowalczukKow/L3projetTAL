7b93bff08d0c43609455dced8f24c282b61f8d88
KowalczukKow <jakub.kowalczukp@gmail.com>
2026-03-02 14:03:56 +0100

    ajout de compteur des phrases

--- fonctions.py
+++ fonctions.py
@@ -1,21 +1,34 @@
+import re
 
 index = {} # nous permet de stocker des informations sur les mots 
 
-def createur_index(valides) :
+def affiche_index():
+    for motIndex in index :
+        print(motIndex,' : ', index[motIndex])
 
+def createur_index(valides) :
+    n_phrase = 1
     for m in valides :
         mot, tag = m.split('/') #pour séparer le mot de sa classe grammaticale
 
         if(tag!='NPP') : mot = mot.lower() # S'il s'agit pas d'un nom propre, 
                                         # le mot sera en minuscules   
-
         if mot not in index:
             index[mot] = {
                 'tags' : [],
-                'nb' : 0
+                'nb' : 0,
+                'n_phrase' : []
             }
 
         index[mot]['tags'].append(tag)
         index[mot]['nb'] +=1
+        index[mot]['n_phrase'].append(n_phrase)
 
-    print(index)
+        if tag== 'PONCT' and mot in['.','!','?'] : 
+            n_phrase+=1
+
+    nb_phrases = n_phrase
+    affiche_index()
+    print("Nombre de phrases", nb_phrases)
+    print("Nombre de formes (ponct inclus) : ", len(index))
+    
