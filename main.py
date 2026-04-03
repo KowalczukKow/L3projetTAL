ff5328d8f1e8cdb0b0d06f0a1eb0cdb5cf8255d1
KowalczukKow <jakub.kowalczukp@gmail.com>
2026-04-03 21:10:33 +0200

    supression du fichier fonctions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
 from reconaissance import expr
-import fonctions
 from corpusStats import CorpusStats
 
 def main():
@@ -9,21 +8,7 @@ def main():
 
     stats = CorpusStats("corpus/small.brown")
     stats.read_corpus(automate, True)
-    # verifie si les mots étaient reconnus
-    # with open("corpus/small.brown", "r", encoding="utf-8") as f:
-    #     contenu = f.read()
-    #     valides = automate.findall(contenu)
-    #     motsTotal = contenu.split()
-    #     print("Mots reconnus: "+ str(len(valides)))
-    #     print("Mots en total: "+str(len(motsTotal)))
-    #     pourcentage = len(valides)/len(motsTotal) * 100
-    #     print("Pourcentage des mots bien annotés: " + str(pourcentage) + "%")
-   
-    # fonctions.createur_index(valides)
-    # fonctions.update_index()
-    # #fonctions.affiche_index()
-    # fonctions.loi_zipf_graphe()
-    # fonctions.requete_mot()
+    
 
 if __name__ == "__main__":
     main()
